3f7ae97bc3772a7cf3f264fb2589d6defa19f623
saminaliza <samshopping27@gmail.com>
2020-01-12 12:37:19 -0500

    Fibonacci and Duplicate words added

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -7,6 +7,20 @@ public class Fibonacci {
           0,1,1,2,3,5,8,13
          */
 
-
+int limit = 40;
+for (int i = 1; i < limit; i++){
+    System.out.println(findFibonacci(i) + ", ");
+}
+    }
+    public static int findFibonacci (int number){
+        if (number == 0){
+            return 1;
+        }
+        if (number == 1){
+            return 1;
+        }if (number == 2){
+            return 3;
+        }
+        return findFibonacci(number-1) + findFibonacci(number-2);
     }
 }
--- src/string/problems/DuplicateWord.java
+++ src/string/problems/DuplicateWord.java
@@ -13,6 +13,28 @@ public class DuplicateWord {
 
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
 
+        int count;
+
+        String st2 = st.toLowerCase();
+
+        String words[] = st2.split("\\W+");
+
+        System.out.println("Duplicate words in a given string are : ");
+        for (int i = 0; i < words.length; i++) {
+            count = 1;
+            for (int j = i + 1; j < words.length; j++) {
+                if (words[i].equals(words[j])) {
+                    count++;
+                    words[j] = "0";
+                }
+            }
+
+            if (count > 1 && words[i] != "0")
+                System.out.println(words[i] + ": " + count + " times");
+        }
+
     }
 
+
 }
+
