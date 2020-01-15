128f53eaa38d51aa1fbec470ea564c01f9cc7249
saminaliza <samshopping27@gmail.com>
2020-01-15 15:16:28 -0500

    String problem added

--- src/string/problems/Permutation.java
+++ src/string/problems/Permutation.java
@@ -12,5 +12,23 @@ public class Permutation {
          * Write Java program to compute all Permutation of a String
          *
          */
+        String str = "ABC";
+        printPermutations(str, "");
+    }
+
+    static void printPermutations(String str, String rest) {
+
+        if (str.length() == 0) {
+
+            System.out.print(rest + " ");
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+            String t = str.substring(0, i) + str.substring(i + 1);
+            printPermutations(t, rest + ch);
+        }
     }
 }
